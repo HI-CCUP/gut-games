63683ff6017277309898c276d06367257a876454
Maciej Zawada <maciejkarolzawada@gmail.com>
2026-01-26 19:46:53 +0100

    Fix frontend-backend connection v3

--- react-app/src/pages/Login.jsx
+++ react-app/src/pages/Login.jsx
@@ -1,3 +1,5 @@
+console.log("API URL:", process.env.REACT_APP_API_URL);
+
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
