daef18e4e8c26892c7deff23d2aec57bd36860ca
Maciej Zawada <maciejkarolzawada@gmail.com>
2026-01-30 16:17:56 +0100

    Fix frontend-backend connection v10 [jestem debilem]

--- react-app/server/src/server.js
+++ react-app/server/src/server.js
@@ -4,15 +4,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
 connectDB();
 
-if (process.env.VERCEL_ENV) {
-  export default app;
-} else {
+const PORT = process.env.PORT || 5000;
+
+if (!process.env.VERCEL) {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
 
+export default app;
